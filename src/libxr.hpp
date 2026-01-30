969aebaf09c149cf5c20095ad1e27f5a3082e75b
molqzone <toiperus@sohu.com>
2026-01-30 20:19:38 +0800

    feat(debug): add jtag gpio bit-bang backend

--- src/libxr.hpp
+++ src/libxr.hpp
@@ -34,6 +34,8 @@
 #include "timebase.hpp"
 #include "timer.hpp"
 #include "transform.hpp"
+#include "driver/debug/jtag.hpp"
+#include "driver/debug/jtag_general_gpio.hpp"
 
 /**
  * @brief LibXR 命名空间
