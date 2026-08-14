8e8ab80ed1b793fafac2424027ce31d40cfd2a38
molqzone <toiperus@sohu.com>
2026-08-14 21:02:31 +0800

    Squashed 'src/driver/ethercat/' changes from bae32ad..0643902
    0643902 Move EtherCAT slave core into source file
    
    git-subtree-dir: src/driver/ethercat
    git-subtree-split: 06439029934e6fed1120544eda9f3a32cd1c563e

--- slave_class.hpp
+++ slave_class.hpp
@@ -10,17 +10,10 @@ namespace LibXR::EtherCAT
 
 class SlaveCore;
 
-namespace Detail
-{
-struct CallbackBridge;
-}
-
 /**
  * EtherCAT device-class base, analogous to LibXR::USB::DeviceClass.
  *
- * Derive from SlaveClass to implement application and board-driver hooks. A
- * SlaveCore constructed with this completed object owns SOES startup and IRQ
- * dispatch.
+ * Derive from SlaveClass to implement application and board-driver hooks.
  */
 class SlaveClass
 {
@@ -98,91 +91,4 @@ class SlaveClass
   friend class SlaveCore;
 };
 
-/**
- * SOES runtime core, analogous to LibXR::USB::DeviceCore.
- *
- * Construct this after the SlaveClass-derived device object. SOES uses global
- * state, so one SlaveCore can be active in an image at a time.
- */
-class SlaveCore final
-{
- public:
-  struct Options
-  {
-    int watchdog_count = 0;
-  };
-
-  explicit SlaveCore(SlaveClass& slave);
-  SlaveCore(SlaveClass& slave, const Options& options);
-
-  SlaveCore(const SlaveCore&) = delete;
-  SlaveCore& operator=(const SlaveCore&) = delete;
-  SlaveCore(SlaveCore&&) = delete;
-  SlaveCore& operator=(SlaveCore&&) = delete;
-
-  ~SlaveCore();
-
-  /** Dispatch a low-priority ESC interrupt event mask from the board driver. */
-  void HandleInterrupt(uint32_t event_mask);
-
-  [[nodiscard]] const Options& GetOptions() const { return options_; }
-
- private:
-  friend struct Detail::CallbackBridge;
-
-  void DispatchSetDefaults() { slave_.OnSetDefaults(); }
-  void DispatchPreStateChange(uint8_t& state, uint8_t& notification)
-  {
-    slave_.OnPreStateChange(state, notification);
-  }
-  void DispatchPostStateChange(uint8_t& state, uint8_t& notification)
-  {
-    slave_.OnPostStateChange(state, notification);
-  }
-  void DispatchApplication() { slave_.OnApplication(); }
-  void DispatchSafeOutputs() { slave_.OnSafeOutputs(); }
-  void DispatchInputs() { slave_.OnInputs(); }
-  void DispatchOutputs() { slave_.OnOutputs(); }
-  void DispatchReceiveProcessData() { slave_.OnReceiveProcessData(); }
-  void DispatchTransmitProcessData() { slave_.OnTransmitProcessData(); }
-  void DispatchEnableInterrupt(uint32_t mask) { slave_.OnEnableInterrupt(mask); }
-  void DispatchDisableInterrupt(uint32_t mask) { slave_.OnDisableInterrupt(mask); }
-  void DispatchEepromEvent() { slave_.OnEepromEvent(); }
-  [[nodiscard]] uint16_t DispatchCheckDistributedClock()
-  {
-    return slave_.OnCheckDistributedClock();
-  }
-  [[nodiscard]] ErrorCode DispatchGetDeviceId(uint16_t& device_id)
-  {
-    return slave_.OnGetDeviceId(device_id);
-  }
-  [[nodiscard]] SlaveClass::ObjectAccessCode DispatchPreObjectDownload(
-      const SlaveClass::ObjectAddress& object, const SlaveClass::ObjectBuffer& buffer)
-  {
-    return slave_.OnPreObjectDownload(object, buffer);
-  }
-  [[nodiscard]] SlaveClass::ObjectAccessCode DispatchPostObjectDownload(
-      const SlaveClass::ObjectAddress& object)
-  {
-    return slave_.OnPostObjectDownload(object);
-  }
-  [[nodiscard]] SlaveClass::ObjectAccessCode DispatchPreObjectUpload(
-      const SlaveClass::ObjectAddress& object, SlaveClass::ObjectBuffer& buffer)
-  {
-    return slave_.OnPreObjectUpload(object, buffer);
-  }
-  [[nodiscard]] SlaveClass::ObjectAccessCode DispatchPostObjectUpload(
-      const SlaveClass::ObjectAddress& object)
-  {
-    return slave_.OnPostObjectUpload(object);
-  }
-
-  inline static SlaveCore* active_ = nullptr;
-
-  SlaveClass& slave_;
-  Options options_{};
-};
-
 }  // namespace LibXR::EtherCAT
-
-#include "detail/slave_class_impl.hpp"
