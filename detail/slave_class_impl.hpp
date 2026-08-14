04bdd77f0bb8a42c583034f7fc2409018298bb54
molqzone <toiperus@sohu.com>
2026-08-14 20:36:12 +0800

    Squashed 'src/driver/ethercat/' changes from 3bcd226..148bf63
    148bf63 Use native C++ EtherCAT slave hooks
    
    git-subtree-dir: src/driver/ethercat
    git-subtree-split: 148bf635713470cb2ed850c75021038d63ba3938

--- detail/slave_class_impl.hpp
+++ detail/slave_class_impl.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-// This file is an implementation detail of slave_class.hpp. It is the only
-// place where XRECAT translates its public Configuration to SOES types.
+// This file is an implementation detail of slave_class.hpp. It owns the C
+// callback bridge and the translation to the SOES stack configuration.
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,35 +13,220 @@ extern "C" {
 namespace LibXR::EtherCAT::Detail
 {
 
+struct CallbackBridge
+{
+  using ObjectAccessCode = SlaveClass::ObjectAccessCode;
+  using ObjectBuffer = SlaveClass::ObjectBuffer;
+
+  static SlaveClass* Active() { return SlaveClass::active_; }
+
+  static void SetDefaults()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnSetDefaults();
+    }
+  }
+
+  static void PreStateChange(uint8_t* state, uint8_t* notification)
+  {
+    if (auto* slave = Active(); slave != nullptr && state != nullptr && notification != nullptr)
+    {
+      slave->OnPreStateChange(*state, *notification);
+    }
+  }
+
+  static void PostStateChange(uint8_t* state, uint8_t* notification)
+  {
+    if (auto* slave = Active(); slave != nullptr && state != nullptr && notification != nullptr)
+    {
+      slave->OnPostStateChange(*state, *notification);
+    }
+  }
+
+  static void Application()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnApplication();
+    }
+  }
+
+  static void SafeOutputs()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnSafeOutputs();
+    }
+  }
+
+  static void Inputs()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnInputs();
+    }
+  }
+
+  static void Outputs()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnOutputs();
+    }
+  }
+
+  static ObjectAccessCode PreObjectDownload(uint16_t index, uint8_t subindex, void* data,
+                                             size_t size, uint16_t flags)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      return slave->OnPreObjectDownload({index, subindex, flags}, {data, size});
+    }
+    return 0;
+  }
+
+  static ObjectAccessCode PostObjectDownload(uint16_t index, uint8_t subindex, uint16_t flags)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      return slave->OnPostObjectDownload({index, subindex, flags});
+    }
+    return 0;
+  }
+
+  static ObjectAccessCode PreObjectUpload(uint16_t index, uint8_t subindex, void* data,
+                                           size_t* size, uint16_t flags)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      ObjectBuffer buffer{data, size == nullptr ? 0u : *size};
+      const ObjectAccessCode code =
+          slave->OnPreObjectUpload({index, subindex, flags}, buffer);
+      if (size != nullptr)
+      {
+        *size = buffer.size;
+      }
+      return code;
+    }
+    return 0;
+  }
+
+  static ObjectAccessCode PostObjectUpload(uint16_t index, uint8_t subindex, uint16_t flags)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      return slave->OnPostObjectUpload({index, subindex, flags});
+    }
+    return 0;
+  }
+
+  static void ReceiveProcessData()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnReceiveProcessData();
+    }
+  }
+
+  static void TransmitProcessData()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnTransmitProcessData();
+    }
+  }
+
+  static void EnableInterrupt(uint32_t mask)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnEnableInterrupt(mask);
+    }
+  }
+
+  static void DisableInterrupt(uint32_t mask)
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnDisableInterrupt(mask);
+    }
+  }
+
+  static void EepromEvent()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      slave->OnEepromEvent();
+    }
+  }
+
+  static uint16_t CheckDistributedClock()
+  {
+    if (auto* slave = Active(); slave != nullptr)
+    {
+      return slave->OnCheckDistributedClock();
+    }
+    return 0;
+  }
+
+  static int GetDeviceId(uint16_t* device_id)
+  {
+    if (auto* slave = Active(); slave != nullptr && device_id != nullptr)
+    {
+      return static_cast<int>(slave->OnGetDeviceId(*device_id));
+    }
+    return static_cast<int>(ErrorCode::PTR_NULL);
+  }
+};
+
 inline esc_cfg_t soes_configuration{};
 
-inline esc_cfg_t MakeSoesConfiguration(const SlaveClass::Configuration& config)
+inline esc_cfg_t MakeSoesConfiguration(const SlaveClass::Options& options)
 {
   esc_cfg_t soes_config{};
-  soes_config.user_arg = config.user_arg;
-  soes_config.use_interrupt = config.use_interrupt ? 1 : 0;
-  soes_config.watchdog_cnt = config.watchdog_count;
-  soes_config.skip_default_initialization = config.skip_default_initialization;
-  soes_config.set_defaults_hook = config.on_set_defaults;
-  soes_config.pre_state_change_hook = config.on_pre_state_change;
-  soes_config.post_state_change_hook = config.on_post_state_change;
-  soes_config.application_hook = config.on_application;
-  soes_config.safeoutput_override = config.on_safe_output;
-  soes_config.pre_object_download_hook = config.on_pre_object_download;
-  soes_config.post_object_download_hook = config.on_post_object_download;
-  soes_config.pre_object_upload_hook = config.on_pre_object_upload;
-  soes_config.post_object_upload_hook = config.on_post_object_upload;
-  soes_config.rxpdo_override = config.on_rxpdo_override;
-  soes_config.txpdo_override = config.on_txpdo_override;
-  soes_config.esc_hw_interrupt_enable = config.on_interrupt_enable;
-  soes_config.esc_hw_interrupt_disable = config.on_interrupt_disable;
-  soes_config.esc_hw_eep_handler = config.on_eeprom_event;
-  soes_config.esc_check_dc_handler = config.on_dc_check;
-  soes_config.get_device_id = config.on_get_device_id;
+  soes_config.user_arg = CallbackBridge::Active();
+  soes_config.use_interrupt = options.execution_mode == SlaveClass::ExecutionMode::INTERRUPT;
+  soes_config.watchdog_cnt = options.watchdog_count;
+  soes_config.skip_default_initialization = options.skip_default_initialization;
+  soes_config.set_defaults_hook = CallbackBridge::SetDefaults;
+  soes_config.pre_state_change_hook = CallbackBridge::PreStateChange;
+  soes_config.post_state_change_hook = CallbackBridge::PostStateChange;
+  soes_config.application_hook = CallbackBridge::Application;
+  soes_config.safeoutput_override = CallbackBridge::SafeOutputs;
+  soes_config.pre_object_download_hook = CallbackBridge::PreObjectDownload;
+  soes_config.post_object_download_hook = CallbackBridge::PostObjectDownload;
+  soes_config.pre_object_upload_hook = CallbackBridge::PreObjectUpload;
+  soes_config.post_object_upload_hook = CallbackBridge::PostObjectUpload;
+  soes_config.rxpdo_override = CallbackBridge::ReceiveProcessData;
+  soes_config.txpdo_override = CallbackBridge::TransmitProcessData;
+  soes_config.esc_hw_interrupt_enable = CallbackBridge::EnableInterrupt;
+  soes_config.esc_hw_interrupt_disable = CallbackBridge::DisableInterrupt;
+  soes_config.esc_hw_eep_handler = CallbackBridge::EepromEvent;
+  soes_config.esc_check_dc_handler = CallbackBridge::CheckDistributedClock;
+  soes_config.get_device_id = CallbackBridge::GetDeviceId;
   return soes_config;
 }
 
 }  // namespace LibXR::EtherCAT::Detail
+
+#if defined(__GNUC__) || defined(__clang__)
+#define LIBXR_ETHERCAT_DETAIL_USED __attribute__((used))
+#else
+#define LIBXR_ETHERCAT_DETAIL_USED
+#endif
+
+extern "C" inline LIBXR_ETHERCAT_DETAIL_USED void cb_get_inputs()
+{
+  LibXR::EtherCAT::Detail::CallbackBridge::Inputs();
+}
+
+extern "C" inline LIBXR_ETHERCAT_DETAIL_USED void cb_set_outputs()
+{
+  LibXR::EtherCAT::Detail::CallbackBridge::Outputs();
+}
+
+#undef LIBXR_ETHERCAT_DETAIL_USED
 
 namespace LibXR::EtherCAT
 {
@@ -66,7 +251,7 @@ inline ErrorCode SlaveClass::Initialize()
   }
 
   active_ = this;
-  Detail::soes_configuration = Detail::MakeSoesConfiguration(config_);
+  Detail::soes_configuration = Detail::MakeSoesConfiguration(options_);
   ecat_slv_init(&Detail::soes_configuration);
   initialized_ = true;
   return ErrorCode::OK;
@@ -110,11 +295,6 @@ inline ErrorCode SlaveClass::Run()
   }
   ecat_slv();
   return ErrorCode::OK;
-}
-
-inline SlaveClass* SlaveClass::Active()
-{
-  return active_;
 }
 
 }  // namespace LibXR::EtherCAT
